e8bab1ee0286d49b209ccbe5662a4c65f02e9d61
Docs Robot <robot@example.com>
2021-07-22 07:12:52 +0000

    Committing documentation changes

--- docs/annotated_dup.js
+++ docs/annotated_dup.js
@@ -1,9 +1,10 @@
 var annotated_dup =
 [
     [ "Repository Contents", "index.html#autotoc_md1", null ],
-    [ "Hardware", "index.html#autotoc_md2", null ],
+    [ "Hardware design", "index.html#autotoc_md2", null ],
     [ "Documentation", "index.html#autotoc_md3", null ],
-    [ "Open-source license", "index.html#autotoc_md4", null ],
+    [ "About Soldered", "index.html#autotoc_md4", null ],
+    [ "Open-source license", "index.html#autotoc_md5", null ],
     [ "EasyC", "class_easy_c.html", "class_easy_c" ],
     [ "Obstacle_Sensor", "class_obstacle___sensor.html", "class_obstacle___sensor" ]
 ];
