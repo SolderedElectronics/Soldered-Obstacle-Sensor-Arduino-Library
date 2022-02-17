64a76953a2b0390724f42ead3195f04de71c4f92
Docs Robot <robot@example.com>
2022-02-17 12:12:47 +0000

    Committing documentation changes

--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,7 +1,6 @@
 var hierarchy =
 [
-    [ "EasyC", "class_easy_c.html", null ],
-    [ "OBSTACLE_SOLDERED_LIBS::EasyC", null, [
+    [ "EasyC", "class_easy_c.html", [
       [ "Obstacle_Sensor", "class_obstacle___sensor.html", null ]
     ] ]
 ];
