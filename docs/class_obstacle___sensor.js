947c9621937f43a3a407c5524f5cc3c33a5b6970
Docs Robot <robot@example.com>
2022-03-23 07:39:25 +0000

    Committing documentation changes

--- docs/class_obstacle___sensor.js
+++ docs/class_obstacle___sensor.js
@@ -3,7 +3,7 @@ var class_obstacle___sensor =
     [ "analogRead", "class_obstacle___sensor.html#a503e4bf3c1d8906b7ad2b20cde79080d", null ],
     [ "digitalRead", "class_obstacle___sensor.html#aca34c61b59ec3053430a90158301d711", null ],
     [ "initializeNative", "class_obstacle___sensor.html#ad7c1efea3f7752a827d1706031034eb7", null ],
-    [ "setTreshold", "class_obstacle___sensor.html#a9b0319b2e4d2b3698bb10d3dab048a7b", null ],
+    [ "setTreshold", "class_obstacle___sensor.html#a2a915e9ee9aa4c6182737c873dd3ae9e", null ],
     [ "pin", "class_obstacle___sensor.html#ab011bb6c8c7dbe97850c70e5b287b976", null ],
     [ "treshold", "class_obstacle___sensor.html#aa6203e33ac2f3a015e1e61441fab1a00", null ]
 ];
--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['sendaddress_0',['sendAddress',['../class_easy_c.html#a10aa7df19e8c7bf12cd720b662f317a3',1,'EasyC']]],
   ['senddata_1',['sendData',['../class_easy_c.html#afe8aa014ae53cad3b0f3c42f59277195',1,'EasyC']]],
-  ['settreshold_2',['setTreshold',['../class_obstacle___sensor.html#a9b0319b2e4d2b3698bb10d3dab048a7b',1,'Obstacle_Sensor']]]
+  ['settreshold_2',['setTreshold',['../class_obstacle___sensor.html#a2a915e9ee9aa4c6182737c873dd3ae9e',1,'Obstacle_Sensor']]]
 ];
